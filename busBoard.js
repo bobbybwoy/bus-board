d6bc2aa2ab57d431999fb9ce471fceaed08e94b9
Ruksan Emirali <ruksan.emirali@softwire.com>
2024-07-03 14:42:31 +0100

    buses sorted by time

--- busBoard.js
+++ busBoard.js
@@ -1,8 +1,10 @@
 fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
     .then(response => response.json())
     .then(body => {
+        let sortBody = body.sort((obj1, obj2) => obj1.timeToStation - obj2.timeToStation)
         for (let i = 0; i < 5; i++) {
-            console.log(body[i].destinationName)
+            console.log(sortBody[i].destinationName)
+            console.log(Math.round(sortBody[i].timeToStation / 60))
         }
     });
 
