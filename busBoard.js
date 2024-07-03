19843e4eea438248b3440b641518faf3fab7b0cc
Ruksan Emirali <ruksan.emirali@softwire.com>
2024-07-03 12:29:01 +0100

    shows next 5 bus destinations

--- busBoard.js
+++ busBoard.js
@@ -1,3 +1,8 @@
 fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
     .then(response => response.json())
-    .then(body => console.log(body));
+    .then(body => {
+        for (let i = 0; i < 5; i++) {
+            console.log(body[i].destinationName)
+        }
+    });
+
