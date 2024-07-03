04301886d2dba90f23125b4c8557a41e57eec4ef
Robert De Leon <robert.deleon@softwire.com>
2024-07-03 15:02:22 +0100

    Tidied up the output

--- busBoard.js
+++ busBoard.js
@@ -3,8 +3,9 @@ fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
     .then(body => {
         let sortBody = body.sort((obj1, obj2) => obj1.timeToStation - obj2.timeToStation)
         for (let i = 0; i < 5; i++) {
-            console.log(sortBody[i].destinationName)
-            console.log(Math.round(sortBody[i].timeToStation / 60))
+            console.log(`Destination: ${sortBody[i].destinationName}`)
+            console.log(`Route      : ${sortBody[i].lineId}`)
+            console.log(`Time arrive: ${Math.round(sortBody[i].timeToStation / 60)} mins\n`)
         }
     });
 
