e5ffc1213cac67a1b5ddc0bcdbb843ff2dcb4251
Ruksan Emirali <ruksan.emirali@softwire.com>
2024-07-04 15:52:53 +0100

    includes giving directions

--- busStops.js
+++ busStops.js
@@ -15,13 +15,14 @@ const logger = winston.createLogger({
 let locationData = 1;
 let busStops;
 let busInfo;
+let nearestStops = []
 while (typeof (locationData) != "string") {
     try {
         // Get the postcode from the user via the terminal
         const postCode = readline.prompt({ prompt: "Postcode: " });
         const postresponse = await fetch(`https://api.postcodes.io/postcodes/${postCode}`);
         if (postresponse.status !== 200) {
-            logger.error(`Postcode API error - status ${postresponse.status}`);
+            logger.error(`Postcode API error - status ${postresponse.status}, Postcode: ${postCode}`);
             throw `Postcode API error - status ${postresponse.status}`
         }
         locationData = await postresponse.json();
@@ -29,18 +30,44 @@ while (typeof (locationData) != "string") {
         let long = locationData.result.longitude
         const tflresponse =
             await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${long}&stopTypes=NaptanPublicBusCoachTram`)
-        if (tflresponse.status !== 200)
+        if (tflresponse.status !== 200){
+            logger.error(`TfL API error - status ${tflresponse.status}`);
             throw `TfL API error - status ${tflresponse.status}`
+        }
         busStops = await tflresponse.json()
         for (let i = 0; i < 2; i++) {
             const busInfoResp =
                 await fetch(`https://api.tfl.gov.uk/StopPoint/${busStops.stopPoints[i].naptanId}/Arrivals`);
-            if (busInfoResp.status !== 200) throw `Bus Info Response error: ${busInfoResp.status}`;
+            if (busInfoResp.status !== 200) {
+                logger.error(`Bus Info Response error: ${busInfoResp.status}`);
+                throw `Bus Info Response error: ${busInfoResp.status}`;
+            }
             const busInfo = await busInfoResp.json();
-            if (busInfo.length == 0) throw `No stop information for ${postCode}`
+            if (busInfo.length == 0) {
+                logger.error(`No stop information for ${postCode}`);
+                throw `No stop information for ${postCode}`
+            }
             const sortedBusInfo = busInfo.sort((bus1, bus2) => bus1.timeToStation - bus2.timeToStation);
+            nearestStops.push(busStops.stopPoints[i].commonName)
             console.log(`Next bus at stop ${busStops.stopPoints[i].commonName} is ${sortedBusInfo[0].lineId} arriving in ${Math.round(sortedBusInfo[0].timeToStation / 60)} mins\n`);
-        } break
+        }
+;
+        // Ask question...
+        const directions = (readline.prompt({prompt: "Do you want directions? Y/N "})).toLowerCase();
+        if (directions != "y" && directions != "n")
+            throw `Do not understand this response ${directions}. Enter Y or N`;
+        else if (directions == "y"){
+            for (let j = 0; j < nearestStops.length; j++) {
+                const directionsResp = await fetch(`https://api.tfl.gov.uk/Journey/JourneyResults/${postCode}/to/${nearestStops[j]}`)
+            if (directionsResp.status != 200) {
+                logger.error(`Unable to direct from ${postCode} to ${nearestStops[j]}`)
+                throw `Unable to direct from ${postCode} to ${nearestStops[j]}`
+            }
+            console.log(`Directions from ${postCode} to ${nearestStops[j]} are `)
+            }
+
+        }
+        break
     } catch (err) {
         console.log(err)
     }     
